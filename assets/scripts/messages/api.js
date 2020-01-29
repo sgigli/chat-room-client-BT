dc6e70c82f8a3611032be6847ef4467f2f6d7c6a
sgigli <simon.luciano@gigli.net>
2020-01-29 12:16:11 -0500

    Delete button working but lots to tweak:
    -delete button should only show for users messages
    -should refresh messages

--- assets/scripts/messages/api.js
+++ assets/scripts/messages/api.js
@@ -26,7 +26,18 @@ const index = () => {
   })
 }
 
+const destroy = (id) => {
+  return $.ajax({
+    url: config.apiUrl + '/messages/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
 module.exports = {
   create,
-  index
+  index,
+  destroy
 }
--- assets/scripts/messages/messageEvents.js
+++ assets/scripts/messages/messageEvents.js
@@ -3,16 +3,15 @@ import io from 'socket.io-client'
 const socket = io('http://localhost:4741')
 const api = require('./api')
 const store = require('../store')
+const getMessagesHtml = require('../templates/messages-listing.handlebars')
 
 const sendMessage = (event) => {
   event.preventDefault() // prevents page reloading
   // socket.emit('chat message', $('#m').val())
   const msg = $('#m').val()
-  console.log(store.user.username)
   const username = store.user.username
   api.create(msg, username)
     .then(res => {
-      console.log(res.message.username)
       $('#messages').append($('<li>').text(`${username}: ${msg}`))
       socket.emit('chat message', `${username}: ${res.message.text}`)
     })
@@ -24,7 +23,16 @@ const getMessages = () => {
   api.index()
     .then(res => {
       const filteredForUsername = res.messages.filter(msg => msg.username)
-      filteredForUsername.forEach(msg => $('#messages').append($('<li>').text(`${msg.username}: ${msg.text}`)))
+      filteredForUsername.forEach(msg => console.log(msg._id))
+      const showMessagesHtml = getMessagesHtml({ messages: filteredForUsername })
+      $('#messages').append(showMessagesHtml)
+      // const filteredForUsername = res.messages.filter(msg => msg.username)
+      // filteredForUsername.forEach(msg => $('#messages').append($('<li>').text(`${msg.username}: ${msg.text}`)))
+
+      // res.messages.forEach(msg => {
+      //   const name = msg.username ? `${msg.username}:` : ''
+      //   $('#messages').append($('<li>').text(`${name} ${msg.text}`))
+      // })
       // res.messages.forEach(msg => {
       //   if (msg.username) {
       //     $('#messages').append($('<li>').text(`${msg.username}: ${msg.text}`))
@@ -35,6 +43,14 @@ const getMessages = () => {
     })
 }
 
+const onDelete = (event) => {
+  event.preventDefault()
+  console.log($(event.target).data('id'))
+  const id = $(event.target).data('id')
+  api.destroy(id)
+    .then(console.log('success'))
+}
+
 const addHandlers = () => {
   // sendMessage()
   getMessages()
@@ -42,6 +58,7 @@ const addHandlers = () => {
   socket.on('chat message', function (msg) {
     $('#messages').append($('<li>').text(msg))
   })
+  $('#messages').on('click', '.delete', onDelete)
 }
 
 module.exports = {
