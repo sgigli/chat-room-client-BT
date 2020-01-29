97dc6236e13d4c74821306a48ec65a50edd4793b
sgigli <simon.luciano@gigli.net>
2020-01-29 14:21:05 -0500

    Delete working for posted messages too

--- assets/scripts/messages/messageEvents.js
+++ assets/scripts/messages/messageEvents.js
@@ -4,15 +4,17 @@ const socket = io('http://localhost:4741')
 const api = require('./api')
 const store = require('../store')
 const getMessagesHtml = require('../templates/messages-listing.handlebars')
+const getPostHtml = require('../templates/post-message.handlebars')
 
 const sendMessage = (event) => {
   event.preventDefault() // prevents page reloading
-  // socket.emit('chat message', $('#m').val())
   const msg = $('#m').val()
   const username = store.user.username
   api.create(msg, username)
     .then(res => {
-      $('#messages').append($('<li>').text(`${username}: ${msg}`))
+      const showPostHtml = getPostHtml({ msg: res.message, id: store.user._id })
+      $('#messages').append(showPostHtml)
+      // $('#messages').append($('<li>').text(`${username}: ${msg}`))
       socket.emit('chat message', `${username}: ${res.message.text}`)
     })
   $('#m').val('')
@@ -23,29 +25,14 @@ const getMessages = () => {
   api.index()
     .then(res => {
       const filteredForUsername = res.messages.filter(msg => msg.username)
-      // filteredForUsername.forEach(msg => console.log(msg._id))
-      console.log(store.user._id)
+      filteredForUsername.forEach(msg => { msg.editable = msg.owner === store.user._id || false })
       const data = {
         messages: filteredForUsername,
         id: store.user._id
       }
-      // { messages: filteredForUsername }
       const showMessagesHtml = getMessagesHtml(data)
+      $('#messages').text('')
       $('#messages').append(showMessagesHtml)
-      // const filteredForUsername = res.messages.filter(msg => msg.username)
-      // filteredForUsername.forEach(msg => $('#messages').append($('<li>').text(`${msg.username}: ${msg.text}`)))
-
-      // res.messages.forEach(msg => {
-      //   const name = msg.username ? `${msg.username}:` : ''
-      //   $('#messages').append($('<li>').text(`${name} ${msg.text}`))
-      // })
-      // res.messages.forEach(msg => {
-      //   if (msg.username) {
-      //     $('#messages').append($('<li>').text(`${msg.username}: ${msg.text}`))
-      //   } else {
-      //     $('#messages').append($('<li>').text(msg.text))
-      //   }
-      // })
     })
 }
 
@@ -55,6 +42,7 @@ const onDelete = (event) => {
   const id = $(event.target).data('id')
   api.destroy(id)
     .then(console.log('success'))
+    .then(getMessages)
 }
 
 const addHandlers = () => {
