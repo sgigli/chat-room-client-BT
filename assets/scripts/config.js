6f9e36068b336bb5185c3a7fa8d8c2d6ee28bfad
sgigli <simon.luciano@gigli.net>
2020-01-30 16:58:47 -0500

    Fixed heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/agile-citadel-40916.git',
+  production: 'https://agile-citadel-40916.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
